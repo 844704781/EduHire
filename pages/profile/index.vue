25313b3ab79fb67cf01dd1528f313201aae67c82
watermelon <844704781@qq.com>
2024-09-23 21:24:28 +0800

    update

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,6 +1,6 @@
 <template>
-	<view>
-		<view class="info">
+	<view class='profile'>
+		<view class="info ">
 			<view class="me">
 				<u-row>
 					<u-col :span='3'>
@@ -46,7 +46,7 @@
 				</u-row>
 			</view>
 		</view>
-		<view class="recruit_now">
+		<view class="recruit_now ">
 			<u-row>
 				<u-col span="2">
 					<u-icon name="plus-people-fill" color="#f9ae3d" size="45"></u-icon>
@@ -60,66 +60,73 @@
 				<u-col span="3"><u-button size='small' type="warning" shape="circle" text="去招聘"></u-button></u-col>
 			</u-row>
 		</view>
-		<view class="common_tools">
+		<view class="common_tools ">
 			<view class="title">常用功能</view>
 			<view class="tools">
 				<view class="item">
 					<view class="logo">
-						<u--image :showLoading="true" src="https://wallpaper.cospapa.cn/oss/v1/get?bucket=eduhire&filename=tools/profile.png" :width='logo_size'
-							:height='logo_size'></u--image>
+						<u--image :showLoading="true"
+							src="https://wallpaper.cospapa.cn/oss/v1/get?bucket=eduhire&filename=tools/profile.png"
+							:width='logo_size' :height='logo_size'></u--image>
 					</view>
 					<view class="name">我的简历</view>
 				</view>
 				<view class="item">
 					<view class="logo">
-						<u--image :showLoading="true" src="https://wallpaper.cospapa.cn/oss/v1/get?bucket=eduhire&filename=tools/profile-file.png" :width='logo_size'
-							:height='logo_size'></u--image>
+						<u--image :showLoading="true"
+							src="https://wallpaper.cospapa.cn/oss/v1/get?bucket=eduhire&filename=tools/profile-file.png"
+							:width='logo_size' :height='logo_size'></u--image>
 					</view>
 					<view class="name">附件简历</view>
 				</view>
 			</view>
 		</view>
-		<view class="common_tools">
+		<view class="common_tools ">
 			<view class="title">其他功能</view>
 			<view class="tools">
 				<view class="item">
 					<view class="logo">
-						<u--image :showLoading="true" src="https://wallpaper.cospapa.cn/oss/v1/get?bucket=eduhire&filename=tools/settings.png" :width='logo_size'
-							:height='logo_size'></u--image>
+						<u--image :showLoading="true"
+							src="https://wallpaper.cospapa.cn/oss/v1/get?bucket=eduhire&filename=tools/settings.png"
+							:width='logo_size' :height='logo_size'></u--image>
 					</view>
 					<view class="name">设置</view>
 				</view>
 				<view class="item">
 					<view class="logo">
-						<u--image :showLoading="true" src="https://wallpaper.cospapa.cn/oss/v1/get?bucket=eduhire&filename=tools/browsing-history.png" :width='logo_size'
-							:height='logo_size'></u--image>
+						<u--image :showLoading="true"
+							src="https://wallpaper.cospapa.cn/oss/v1/get?bucket=eduhire&filename=tools/browsing-history.png"
+							:width='logo_size' :height='logo_size'></u--image>
 					</view>
 					<view class="name">浏览记录</view>
 				</view>
 				<view class="item">
 					<view class="logo">
-						<u--image :showLoading="true" src="https://wallpaper.cospapa.cn/oss/v1/get?bucket=eduhire&filename=tools/business-cooperation.png" :width='logo_size'
-							:height='logo_size'></u--image>
+						<u--image :showLoading="true"
+							src="https://wallpaper.cospapa.cn/oss/v1/get?bucket=eduhire&filename=tools/business-cooperation.png"
+							:width='logo_size' :height='logo_size'></u--image>
 					</view>
 					<view class="name">商务合作</view>
 				</view>
 				<view class="item">
 					<view class="logo">
-						<u--image :showLoading="true" src="https://wallpaper.cospapa.cn/oss/v1/get?bucket=eduhire&filename=tools/chat-support.png" :width='logo_size'
-							:height='logo_size'></u--image>
+						<u--image :showLoading="true"
+							src="https://wallpaper.cospapa.cn/oss/v1/get?bucket=eduhire&filename=tools/chat-support.png"
+							:width='logo_size' :height='logo_size'></u--image>
 					</view>
 					<view class="name">在线客服</view>
 				</view>
 				<view class="item">
 					<view class="logo">
-						<u--image :showLoading="true" src="https://wallpaper.cospapa.cn/oss/v1/get?bucket=eduhire&filename=tools/feedback.png" :width='logo_size'
-							:height='logo_size'></u--image>
+						<u--image :showLoading="true"
+							src="https://wallpaper.cospapa.cn/oss/v1/get?bucket=eduhire&filename=tools/feedback.png"
+							:width='logo_size' :height='logo_size'></u--image>
 					</view>
 					<view class="name">问题反馈</view>
 				</view>
 			</view>
 		</view>
-		<view class="exit">
+		<view class="exit ">
 			<view class="exit_button">
 				<u-button :plain="true" shape="circle" type='error' text="退出登录"></u-button>
 			</view>
@@ -141,6 +148,9 @@
 				}
 			}
 		},
+		onLoad() {
+
+		},
 		methods: {
 
 		}
@@ -148,120 +158,144 @@
 </script>
 
 <style lang="scss">
-	.info {
-		.me {
-			// border: solid 1px #b1b1b1;
-			padding: 0 10rpx;
-			.name {
-				font-size: 50rpx;
-				margin: 10rpx 0;
-			}
-
-			.status {
-				width: 190rpx;
+	.profile {
 
 
-				/deep/ .u-icon__icon span {
-					font-size: 15rpx;
-				}
-
-				/deep/ .u-button__text span {
-					font-size: 17rpx;
-					color: #f9ae3d;
-				}
-			}
-		}
-
-		.overview {
-			border: solid 1px #e5e5e5;
-			margin: 20rpx 30rpx;
-			border-radius: 20rpx;
-
-			.item {
-				text-align: center;
-
-				.count {
-					// border: solid 1px #b1b1b1;
-					font-size: 38rpx;
-					font-weight: 500;
-					padding: 10rpx;
-				}
+		.info {
+			.me {
+				// border: solid 1px #b1b1b1;
+				padding: 0 10rpx;
 
 				.name {
-					// border: solid 1px #b1b1b1;
-					font-size: 20rpx;
-					font-weight: 400;
-					color: #b1b1b1;
-					padding: 10rpx;
-				}
-			}
-		}
-
-	}
-
-	.recruit_now {
-		border: solid 1px #e5e5e5;
-		margin: 0 30rpx;
-		padding: 15rpx 10rpx;
-		border-radius: 20rpx;
-
-		.title1 {
-			font-size: 25rpx;
-			font-weight: 100;
-			margin: 10rpx 0;
-		}
-
-		.title2 {
-			font-size: 20rpx;
-			font-weight: 50;
-			margin: 10rpx 0;
-		}
-	}
-
-	.common_tools {
-		border: solid 1px #e5e5e5;
-		margin: 10rpx 30rpx;
-		padding: 15rpx 10rpx;
-		border-radius: 20rpx;
-
-		.title {
-			font-size: 30rpx;
-			font-weight: 1000;
-		}
-
-		.tools {
-			display: flex;
-			flex-wrap: wrap;
-			margin: 20rpx 0 0 0;
-
-			.item {
-				// border: solid 1px red;
-				width: 25%;
-				margin: 10rpx 0;
-				// padding: 10rpx;
-				.logo {
-					// border: solid 1px red;
-					display: flex;
-					justify-content: center;
-				}
-
-				.name {
-					// border: solid 1px red;
-					font-size: 20rpx;
-					text-align: center;
-					color: #b1b1b1;
+					font-size: 50rpx;
 					margin: 10rpx 0;
 				}
+
+				.status {
+					uni-button {
+						display: inline-flex;
+						width: auto;
+						white-space: nowrap;
+						overflow: hidden;
+						/* 防止子元素换行 */
+						background: #f9ae3d;
+						border-radius: 50rpx;
+
+					}
+
+					/deep/ .u-icon__icon span {
+						display: inline-flex;
+						font-size: 10rpx;
+						color: #ffffff;
+
+					}
+
+					/deep/ .u-button__text span {
+						display: inline-flex;
+						font-size: 15rpx;
+	
+						color: #ffffff;
+
+					}
+				}
+			}
+
+			.overview {
+				border: solid 1px #e5e5e5;
+				margin: 20rpx 30rpx;
+				border-radius: 20rpx;
+
+				.item {
+					text-align: center;
+
+					.count {
+						// border: solid 1px #b1b1b1;
+						font-size: 38rpx;
+						font-weight: 500;
+						padding: 10rpx;
+					}
+
+					.name {
+						// border: solid 1px #b1b1b1;
+						font-size: 20rpx;
+						font-weight: 400;
+						color: #b1b1b1;
+						padding: 10rpx;
+					}
+				}
+			}
+
+		}
+
+		.recruit_now {
+			border: solid 1px #e5e5e5;
+			margin: 30rpx 30rpx;
+			padding: 15rpx 10rpx;
+			border-radius: 20rpx;
+
+			.title1 {
+				font-size: 25rpx;
+				font-weight: 100;
+				margin: 10rpx 0;
+			}
+
+			.title2 {
+				font-size: 20rpx;
+				font-weight: 50;
+				margin: 10rpx 0;
 			}
 		}
-	}
-	.exit{
 
-		display: flex;
-		justify-content: center;
-		margin: 20rpx 0;
-		.exit_button{
-			width: 50%;
+		.common_tools {
+			border: solid 1px #e5e5e5;
+			margin: 30rpx 30rpx;
+			padding: 15rpx 10rpx;
+			border-radius: 20rpx;
+
+			.title {
+				font-size: 30rpx;
+				font-weight: 1000;
+			}
+
+			.tools {
+				display: flex;
+				flex-wrap: wrap;
+				margin: 20rpx 0 0 0;
+
+				.item {
+					// border: solid 1px red;
+					width: 25%;
+					margin: 10rpx 0;
+
+					// padding: 10rpx;
+					.logo {
+						// border: solid 1px red;
+						display: flex;
+						justify-content: center;
+					}
+
+					.name {
+						// border: solid 1px red;
+						font-size: 20rpx;
+						text-align: center;
+						color: #b1b1b1;
+						margin: 10rpx 0;
+					}
+				}
+			}
+		}
+
+		.exit {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			margin: 30rpx 0;
+			height: 300rpx;
+
+			.exit_button {
+				width: 50%;
+			}
 		}
 	}
 </style>
